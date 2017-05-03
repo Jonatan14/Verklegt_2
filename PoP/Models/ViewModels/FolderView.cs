545a615118b46e17128db30e960b6c941434cced
Jonatan14 <jonatan14@ru.is>
2017-05-03 13:45:34 +0000

    some change

---
+++ PoP/Models/ViewModels/FolderView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PoP.Models.ViewModels
+{
+    public class FolderView
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+    }
+}
---
+++ PoP/Service/UserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PoP.Service
+{
+    public class UserService
+    {
+        public List<>getUserId(int ID)
+        {
+
+        }
+    }
+}
