41637932eb835da5e4b2a8ef696afa1c2c27fa14
Alexandermar <donalli89@gmail.com>
2017-05-03 15:12:13 +0000

    AccountModel and FileModel

---
+++ PoP/Models/AccountModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PoP.Models
+{
+    public class AccountModel
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+        public int id { get; set; }
+        public string securityQuestion { get; set; }
+        public string securityAnswer { get; set; }
+    }
+}
--- PoP/Service/FileService.cs
+++ PoP/Service/FileService.cs
@@ -8,5 +8,40 @@ namespace PoP.Service
 {
     public class FileService
     {
+        private FileModel file = null;
+
+        public FileModel getFile(int id)
+        {
+
+            return file;
+        }
+
+        public FileModel updateFile(FileModel file)
+        {
+            return file;
+        }
+
+        public int getFileVersion(FileModel model)
+        {
+            int version = 0;
+            return version;
+        }
+
+        public bool isNewest(FileModel model)
+        {
+            return true;
+        }
+
+        public int activeEditorsCount(FileModel model)
+        {
+            int count = 0;
+            return count;
+        }
+
+        public List<AccountModel>activeEditors()
+        {
+            List<AccountModel> modelList = null;
+            return modelList;
+        }
     }
 }
