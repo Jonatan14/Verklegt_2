fbf68a18f0598d82af17d5befae1fd2237c44c50
Oliprik <oliprik121@gmail.com>
2017-05-03 14:19:27 +0000

    Folder Model added

---
+++ PoP/Models/FolderModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PoP.Models
+{
+	public class FolderModel
+	{
+		public int id { get; set; }
+		public string name { get; set; }
+		public int editedLast { get; set; } // date time!
+		public int ParentFolderID { get; set; } // ID of parent folder
+	}
+}
--- PoP/Service/FolderService.cs
+++ PoP/Service/FolderService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PoP.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,24 @@ namespace PoP.Service
 {
     public class FolderService
     {
+		private FolderModel folder;
+
+		public FolderModel getFolder(int id)
+		{
+			FolderModel result = null;
+
+			return result;
+		}
+		public FolderModel updateFolder(FolderModel file)
+		{
+			FolderModel result = null;
+
+			return result;
+		}
+		public List<FolderModel> filesInDirector(int id)
+		{
+			List<FolderModel> result = null;
+			return result;
+		}
     }
 }
